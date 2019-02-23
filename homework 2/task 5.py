3c04430bb7e0719a8c87ed8132cec6ba3d230939
margaritapetrosova <47389364+margaritapetrosova@users.noreply.github.com>
2019-02-23 17:04:21 +0400

    Delete task 5.py

--- homework 2/task 5.py
+++
@@ -1,7 +0,0 @@
-word1='Smth'
-print(ord('S'), ord('m'), ord('t'), ord('h'))
-word2='Smth'
-print(ord('S'), ord('m'), ord('t'), ord('h'))
-
-euc=((ord('S')-ord('S'))**2+(ord('m')-ord('m'))**2+(ord('t')-ord('t'))**2+(ord('m')-ord('m'))**2)**0.5
-print(euc)
